28c482f98068c00732c1eea9e95ac314f447af7d
Bibek Panthi <bpanthi977@gmail.com>
2025-05-13 16:07:06 -0500

    Merge the iteration and node/element dimension in dataset

--- dataset.py
+++ dataset.py
@@ -21,5 +21,13 @@ def load_dataset(config: DatasetConfig):
 
     train_X = np.array(db[label]['input'], dtype=np.float32)
     train_Y = np.array(db[label]['output'], dtype=np.float32)
+    # X shape is [iterations, nodes or elements, input_vec]
+    # Y shape is [iterations, nodes or elements, output_vec]
 
+    # Merge iterations and node/elements dimension
+    input_dim = train_X.shape[-1]
+    train_X = train_X.reshape([-1, input_dim])
+
+    output_dim = train_Y.shape[-1]
+    train_Y = train_Y.reshape([-1, output_dim])
     return Dataset(train_X, train_Y)
