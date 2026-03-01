2ab64f2d4a360b493d20cbc1dbe5381eca4a3da7
Bibek Panthi <bpanthi977@gmail.com>
2026-03-01 10:26:46 -0600

    Use more vivid color for energy heatmap illustration

--- energy.py
+++ energy.py
@@ -5,6 +5,7 @@ import argparse
 from pathlib import Path
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 
 def read_array(f):
     n_dims = np.fromfile(f, dtype=np.int32, count=1)[0]
@@ -25,6 +26,29 @@ def read_energy_file(path):
             i += 1
 
     return np.array(dataset)
+def create_cmap(e1, e2):
+    colors = [
+        "#440154", # -2: Deep Purple
+        "#3b528b", # -1: Dark Blue
+        "#21918c", #  0: Teal (Unit Error)
+        "#5ec962", #  1: Green
+        "#fde725", #  2: Bright Yellow
+        "#ff9500", #  3: Orange
+        "#ff4400", #  4: Red-Orange
+        "#8b0000"  #  5: Deep Red (Max Magnitude)
+    ]
+
+    # 2. Create the colormap
+    cmap_name = 'magnitude_5_interpolated'
+    custom_cmap = mcolors.LinearSegmentedColormap.from_list(cmap_name, colors, N=256)
+
+    # 3. Use these limits in your plotting function
+    # vmin = min(np.min(e1), np.min(e2))
+    # vmax = max(np.max(e1), np.max(e2))
+    # cmap = 'magma'
+    vmin, vmax = -2, 5
+
+    return custom_cmap, vmin, vmax
 
 def create_heatmap(original, model, t=0, x=0):
     e1 = original[t][x]
@@ -36,11 +60,7 @@ def create_heatmap(original, model, t=0, x=0):
     ediff = model[t][x] - original[t][x]
     ediff = np.log10(np.clip(np.abs(ediff), a_min=1e-3, a_max=1e8))
 
-    vmin = min(np.min(e1), np.min(e2))
-    vmax = max(np.max(e1), np.max(e2))
-
-    cmap = 'viridis'
-
+    cmap, vmin, vmax = create_cmap(e1, e2)
     fig = plt.figure(figsize=(8, 2.5))
     gs = fig.add_gridspec(1, 4, wspace=0.15, width_ratios=[1, 1, 1, 0.05])
     axes = [fig.add_subplot(gs[0, i]) for i in range(3)]
